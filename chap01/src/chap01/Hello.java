1447a20492279bb3195e6dce1743f393a2355a7a
carrie313 <carrie313@naver.com>
2020-05-26 16:05:48 +0900

    home 수정

--- chap01/src/chap01/Hello.java
+++ chap01/src/chap01/Hello.java
@@ -2,6 +2,7 @@ package chap01;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("java!");
+		System.out.println("java!"); 
+		System.out.println("pro"); 
 	}
 }

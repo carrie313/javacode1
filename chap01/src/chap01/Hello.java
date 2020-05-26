5717976b8af615efd19446a7bbd8ca8da00fbf46
carrie313 <carrie313@naver.com>
2020-05-26 17:45:24 +0900

    26일최종

--- chap01/src/chap01/Hello.java
+++ chap01/src/chap01/Hello.java
@@ -2,7 +2,7 @@ package chap01;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("java!"); 
-		System.out.println("pro"); 
-	}
+		System.out.println(Byte.MIN_VALUE);
+		
+}
 }
---
+++ chap01/src/chap01/PromotionExample.java
@@ -0,0 +1,18 @@
+package chap01;
+
+public class PromotionExample {
+
+	public static void main(String[] args) {
+		byte byteValue = 10;
+		int intValue = byteValue; 
+		System.out.println(intValue);
+	
+		char charValue = '°¡';
+		intValue = charValue;
+		
+		byteValue = (byte) intValue;
+}
+
+	}
+
+
